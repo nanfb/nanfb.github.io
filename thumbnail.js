2dca6edb0acd0d49d158164c9e155eefd02e949b
nanfb <56207464+nanfb@users.noreply.github.com>
2022-09-30 03:35:03 +0800

    Update thumbnail.js

--- thumbnail.js
+++ thumbnail.js
@@ -1,12 +1,8 @@
-document.addEventListener("DOMContentLoaded", function (event) {
-        let targetImage = document.querySelector("#smart-image");
-        targetImage.addEventListener("click", function () {
-                 alert("点击了图片");
-                 targetImage.classList.remove("small");
-//             if (targetImage.classList.contains("small")) {
-//                 targetImage.classList.remove("small");
-//             } else {
-//                 targetImage.classList.add("small");
-//             }
-        });
+document.addEventListener("DOMContentLoaded", function(event) {
+    // 你的所有代码都应该写在这里
+    let targetImage = document.querySelector("#smart-image");
+    targetImage.addEventListener("click", function() {
+        alert("点击了图片");
+        targetImage.classList.add("small");
     });
+});
