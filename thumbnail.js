5933300545cdabed63c825186d555b6a3731d4c2
nanfb <56207464+nanfb@users.noreply.github.com>
2022-09-30 03:17:59 +0800

    Update thumbnail.js

--- thumbnail.js
+++ thumbnail.js
@@ -1,3 +1,11 @@
-document.addEventListener("DOMContentLoaded", function(event) {
-    // 你的所有代码都应该写在这里
-});
+document.addEventListener("DOMContentLoaded", function (event) {
+        let targetImage = document.querySelector("#smart-image");
+        targetImage.addEventListener("click", function () {
+            if (targetImage.classList.contains("small")) {
+                targetImage.classList.remove("small");
+            } else {
+                targetImage.classList.add("small");
+
+            }
+        });
+    });
