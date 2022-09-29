788a2aff98d700e6d17d1c8906e140b59d335933
nanfb <56207464+nanfb@users.noreply.github.com>
2022-09-30 03:25:41 +0800

    Update thumbnail.js

--- thumbnail.js
+++ thumbnail.js
@@ -2,11 +2,11 @@ document.addEventListener("DOMContentLoaded", function (event) {
         let targetImage = document.querySelector("#smart-image");
         targetImage.addEventListener("click", function () {
                  alert("点击了图片");
-            if (targetImage.classList.contains("small")) {
-                targetImage.classList.remove("small");
-            } else {
-                targetImage.classList.add("small");
-
-            }
+                 targetImage.classList.remove("small");
+//             if (targetImage.classList.contains("small")) {
+//                 targetImage.classList.remove("small");
+//             } else {
+//                 targetImage.classList.add("small");
+//             }
         });
     });
